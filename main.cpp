a28c987b18e2937cab22dd8fb54069a80bc314a4
garrett1001 <gwelton@mail.csuchico.edu>
2018-02-15 22:05:31 -0800

    Gives lucky number

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors:Garrett Welton
 #include<iostream>
 #include<string>
 
@@ -7,10 +7,27 @@ using namespace std;
 int main()
 {
   //declare variables
+string first;
+string last;
+string nickname;
+int lucky;
 
   //get user input
+cout<<"Enter your first name"<<endl;
+cin>>first;
+cout<<"Enter your last name"<<endl;
+cin>>last;
+
+nickname.push_back(first[0]);
+nickname.push_back(last[0]);
 
   //tell fortune
+lucky=first.length();
+
+cout<<"Welcome, "<<nickname<<", here is your fortune. Your lucky number is "<<lucky<<endl;
+
+
+
 
   return 0;
 }
