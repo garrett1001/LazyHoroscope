97837c94b0d2046aa4741decb95e92684889c512
garrett1001 <gwelton@mail.csuchico.edu>
2018-02-15 23:07:13 -0800

    complete

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ string first;
 string last;
 string nickname;
 int lucky;
+int lastLetter;
 
   //get user input
 cout<<"Enter your first name"<<endl;
@@ -18,14 +19,38 @@ cin>>first;
 cout<<"Enter your last name"<<endl;
 cin>>last;
 
+lucky=first.length();
+lastLetter=last.length()-1;
+
+first[0]=toupper(first[0]);
+last[0]=toupper(last[0]);
+last[lastLetter]=toupper(last[lastLetter]);
+
 nickname.push_back(first[0]);
+nickname.push_back('.');
 nickname.push_back(last[0]);
+nickname.push_back('.');
 
   //tell fortune
-lucky=first.length();
+cout<<"Welcome, "<<nickname<<", here is your fortune..."<<endl;
 
-cout<<"Welcome, "<<nickname<<", here is your fortune. Your lucky number is "<<lucky<<endl;
+cout<<"Your lucky number is "<<lucky<<endl;
 
+if(first[0]=='A' || first[0]=='E' || first[0]=='I' || first[0]=='O' || first[0]=='U')
+{
+cout<<"you are destined to be famous."<<endl;
+}
+else
+{
+cout<<"you should keep a low profile."<<endl;
+}
+
+if(last[lastLetter]=='A' || last[lastLetter]=='E' || last[lastLetter]=='I' || last[lastLetter]=='O' || last[lastLetter]=='U')
+{
+cout<<"you have already meet your true love."<<endl;
+}
+
+cout<<"have a good day!"<<endl;
 
 
 
