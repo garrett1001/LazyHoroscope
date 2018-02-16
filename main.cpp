4a3c5c49fd2af6142b67b575ac71e4703e38827d
garrett1001 <gwelton@mail.csuchico.edu>
2018-02-15 23:13:10 -0800

    complete

--- main.cpp
+++ main.cpp
@@ -14,9 +14,9 @@ int lucky;
 int lastLetter;
 
   //get user input
-cout<<"Enter your first name"<<endl;
+cout<<"What is your first name?"<<endl;
 cin>>first;
-cout<<"Enter your last name"<<endl;
+cout<<"What is your last name?"<<endl;
 cin>>last;
 
 lucky=first.length();
@@ -34,11 +34,11 @@ nickname.push_back('.');
   //tell fortune
 cout<<"Welcome, "<<nickname<<", here is your fortune..."<<endl;
 
-cout<<"Your lucky number is "<<lucky<<endl;
+cout<<"your lucky number is "<<lucky<<endl;
 
 if(first[0]=='A' || first[0]=='E' || first[0]=='I' || first[0]=='O' || first[0]=='U')
 {
-cout<<"you are destined to be famous."<<endl;
+cout<<"you are destined to be famous!"<<endl;
 }
 else
 {
@@ -47,7 +47,7 @@ cout<<"you should keep a low profile."<<endl;
 
 if(last[lastLetter]=='A' || last[lastLetter]=='E' || last[lastLetter]=='I' || last[lastLetter]=='O' || last[lastLetter]=='U')
 {
-cout<<"you have already meet your true love."<<endl;
+cout<<"you have already met your true love."<<endl;
 }
 
 cout<<"have a good day!"<<endl;
